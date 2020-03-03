55b43bbafeaf1dea1a5f2fa45a1c75a514e6d172
Shusuke Nakamura <shuchan.galaxy@gmail.com>
2020-03-03 13:07:36 +0900

    P.104まで学習完了

---
+++ chap02/code2-17.py
@@ -0,0 +1,5 @@
+scores = (70, 80, 55)
+print(scores)
+print(scores[0])
+print('要素数は{}'.format(len(scores)))
+print('合計値は{}'.format(sum(scores)))
---
+++ chap02/code2-20.py
@@ -0,0 +1,2 @@
+members = ('松田')
+print(type(members))
