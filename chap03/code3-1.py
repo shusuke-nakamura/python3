c185392c77c175b7700b41db4205792c2ccff48c
Shusuke Nakamura <shuchan.galaxy@gmail.com>
2020-03-14 20:34:49 +0900

    P.131まで学習完了

---
+++ chap03/code3-1.py
@@ -0,0 +1,4 @@
+name = input('あなたの名前を教えてください>>')
+print('{}さん、こんにちは'.format(name))
+food = input('{}さんの好きな食べ物を教えてください>>'.format(name))
+print('私も{}が好きですよ'.format(food))
---
+++ chap03/code3-2.py
@@ -0,0 +1,8 @@
+name = input('あなたの名前を教えてください>>')
+print('{}さん、こんにちは'.format(name))
+food = input('{}さんの好きな食べ物を教えてください>>'.format(name))
+
+if food == 'カレー':
+  print('素敵です。カレーは最高ですよね！！')
+else:
+  print('私も{}が好きですよ'.format(food))
