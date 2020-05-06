aeca0666326b30a57ff2bdfd674ace809dd97c07
Shusuke Nakamura <shuchan.galaxy@gmail.com>
2020-05-06 17:32:58 +0900

    chap04の学習開始
    P.160

---
+++ chap04/code4-1.py
@@ -0,0 +1,9 @@
+print('さあ、寝ようかしら')
+count = 0  #ひつじの数
+count += 1
+print('ひつじが{}匹'.format(count))
+count += 1
+print('ひつじが{}匹'.format(count))
+count += 1
+print('ひつじが{}匹'.format(count))
+print('おやすみなさい・・・')
