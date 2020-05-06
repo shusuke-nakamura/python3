a9b9841354fd9cb2ea1978b860a1d719042b70a9
Shusuke Nakamura <shuchan.galaxy@gmail.com>
2020-05-06 17:27:13 +0900

    chap03の学習完了

---
+++ chap03/code3-10.py
@@ -0,0 +1,19 @@
+print('すべての質問にyまたはnで答えてください')
+okane_aruka = input('お金に余裕はありますか？＞＞')
+
+if okane_aruka == 'y':
+  onaka_suiteruka = input('お腹がすごく空いてますか？＞＞')
+  nomitai_kibunka = input('ビールを飲みたいですか？＞＞')
+  if onaka_suiteruka == 'y' and nomitai_kibunka == 'y':
+    print('焼肉はいかがですか')
+  elif onaka_suiteruka == 'y':
+    print('カレーはいかがですか')
+  elif nomitai_kibunka == 'y':
+    print('焼き鳥はいかかですか')
+  else:
+    print('パスタはいかがですか')
+  yashoku_iruka = input('夜食は必要ですか？＞＞')
+  if yashoku_iruka == 'y':
+    print('コンビニのチキンはいかがですか') 
+else:
+  print('家で食べましょう')
---
+++ chap03/code3-9.py
@@ -0,0 +1,11 @@
+score = int(input('試験の点数を入力してください>>'))
+
+if score < 0 or score > 100:
+  print('異常な得点です')
+  print('入力し直してください')
+elif score >= 60:
+  print('合格')
+  print('よくがんばりましたね')
+else:
+  print('残念ながら不合格です')
+  print('追試を受けてください')
