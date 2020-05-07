41b73227faf8fddce2a6e9a0fc4ca71c0ab3e8a5
FSI中村 修介 <nakshusu@fsi.co.jp>
2020-05-07 17:00:08 +0900

    P.224 まで学習完了

---
+++ chap05/code5-22-23.py
@@ -0,0 +1,14 @@
+def eat(breakfast, lunch, dinner='カレー'):
+  print('朝は{}を食べました。'.format(breakfast))
+  print('昼は{}を食べました。'.format(lunch))
+  print('晩は{}を食べました。'.format(dinner))
+
+
+print('8月1日')
+eat('トースト', 'おにぎり')
+print('8月2日')
+eat('納豆ごはん', 'ラーメン')
+print('8月3日')
+eat('バナナ', 'そば', '焼肉')
+print('8月4日')
+eat('サンドウィッチ', 'しゅうまい弁当')
