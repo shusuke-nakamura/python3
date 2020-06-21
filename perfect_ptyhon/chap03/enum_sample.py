389125454c36212c7f9d9ea82d38366fdbef6140
Shusuke Nakamura <shuchan.galaxy@gmail.com>
2020-06-21 22:57:05 +0900

    3-8-1 　 メンバー の 列挙 と エイリアス

--- perfect_ptyhon/chap03/enum_sample.py
+++ perfect_ptyhon/chap03/enum_sample.py
@@ -8,3 +8,10 @@ class UserType(Enum):
 print(UserType.GUEST)
 print(UserType.GUEST.name)
 print(UserType.GUEST.value)
+
+class Color(Enum):
+  RED = (255, 0, 0)
+  GREEN = (0, 255, 0)
+  BLUE = (0, 0, 255)
+
+print(Color['RED'])
